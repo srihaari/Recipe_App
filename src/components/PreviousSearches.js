871b25b0e8cfa875d2bd8f69ae1aec2317b13ace
srihaari <insomniac@Sriharis-MacBook-Pro.local>
2022-12-30 00:25:05 +0530

    Completed Recipe App

--- src/components/PreviousSearches.js
+++ src/components/PreviousSearches.js
@@ -1,16 +1,16 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faSearch } from "@fortawesome/free-solid-svg-icons"
 
-export default function PreviousSearches(){
-    const searches = ['pizza', 'burger', 'cookies', 'juice', 'biriyani', 'salad', 'ice cream', 'lasagna', 'pudding', 'soup'] 
+export default function PreviousSearches() {
+    const searches = ['pizza', 'burger', 'cookies', 'juice', 'biriyani', 'salad', 'ice cream', 'lasagna', 'pudding', 'soup']
 
     return (
         <div className="previous-searches section">
             <h2>Previous Searches</h2>
             <div className="previous-searches-container">
-                { searches.map((search, index) => (<div key={index} style={{animationDelay: index * .1 + "s"}} className="search-item">
+                {searches.map((search, index) => (<div key={index} style={{ animationDelay: index * .1 + "s" }} className="search-item">
                     {search}
-                </div>)) }
+                </div>))}
             </div>
             <div className="search-box">
                 <input type="text" placeholder="Search ..." />
